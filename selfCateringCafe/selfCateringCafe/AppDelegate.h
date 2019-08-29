77fe857e4c9a34ec0cb4dd00a73c046f73f11d38
buythelike <1716089236@qq.com>
2019-08-29 10:17:32 +0800

    1.自咖商学院第一次上传

---
+++ selfCateringCafe/selfCateringCafe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  selfCateringCafe
+//
+//  Created by 胡俊杰 on 2019/8/29.
+//  Copyright © 2019 胡俊杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ selfCateringCafe/selfCateringCafe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  selfCateringCafe
+//
+//  Created by 胡俊杰 on 2019/8/29.
+//  Copyright © 2019 胡俊杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
